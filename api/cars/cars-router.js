4718d18e6519f1f5b4e2d14a029def063e1c1958
Kevin MacFadyen <kmacfadyen3@gmail.com>
2023-05-14 23:51:11 -0400

    check two

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,29 @@
 // DO YOUR MAGIC
+const express = require('express')
+const Car = require('./cars-model')
+const {
+    checkCarId,
+} = require('./cars-middleware')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
+})
+
+router.post('/', async (req, res, next) => {
+    res.json('posting new car')
+})
+
+
+module.exports = router
